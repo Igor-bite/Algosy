b0982830d0c34e60a08fc41fe4656030508501bf
Клюжев И. А <klyuzhevigor@gmail.com>
2020-02-18 12:29:30 +0300

    Initial Commit

---
+++ Algosy 8 lab E/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Algosy 8 lab E
+//
+//  Created by Игорь Клюжев on 18.02.2020.
+//  Copyright © 2020 itmo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
